robtaylor/compiz

commit 69c7f0cf084359d666447f4573d7cc8d8b6420d0

Update openGL Plugin documentation

plugins/opengl/include/opengl/opengl.h
@@ -39,7 +39,9 @@
 
 #include <core/pluginclasshandler.h>
 
-/* camera distance from screen, 0.5 * tan (FOV) */
+/**
+ * camera distance from screen, 0.5 * tan (FOV)
+ */
 #define DEFAULT_Z_CAMERA 0.866025404f
 
 #define RED_SATURATION_WEIGHT   0.30f
@@ -236,15 +238,61 @@ class GLScreenInterface :
     public WrapableInterface<GLScreen, GLScreenInterface>
 {
     public:
-	virtual bool glPaintOutput (const GLScreenPaintAttrib &,
-				    const GLMatrix &, const CompRegion &,
-				    CompOutput *, unsigned int);
-	virtual void glPaintTransformedOutput (const GLScreenPaintAttrib &,
-					       const GLMatrix &,
-					       const CompRegion &,
-					       CompOutput *, unsigned int);
-	virtual void glApplyTransform (const GLScreenPaintAttrib &,
-				       CompOutput *, GLMatrix *);
+
+	/**
+	 * Hookable function used for plugins to use openGL to draw on an output
+	 *
+	 * @param attrib Describes some basic drawing attribs for the screen
+	 * including translation, rotation and scale
+	 * @param matrix Describes a 4x4 3D modelview matrix for which this
+	 * screen should be drawn in
+	 * @param region Describes the region of the screen being redrawn
+	 * @param output Describes the output being redrawn
+	 * @param mask   Bitmask which describes how the screen is being redrawn'
+	 */
+	virtual bool glPaintOutput (const GLScreenPaintAttrib &attrib,
+				    const GLMatrix 	      &matrix,
+				    const CompRegion 	      &region,
+				    CompOutput 		      *output,
+				    unsigned int	      mask);
+
+
+	/**
+	 * Hookable function used for plugins to use openGL to draw on an output
+	 * when the screen is transformed
+	 *
+	 * There is little difference between this and glPaintOutput, however
+	 * this will be called when the entire screen is being transformed
+	 * (eg cube)
+	 *
+	 * @param attrib Describes some basic drawing attribs for the screen
+	 * including translation, rotation and scale
+	 * @param matrix Describes a 4x4 3D modelview matrix for which this
+	 * screen should be drawn in
+	 * @param region Describes the region of the screen being redrawn
+	 * @param output Describes the output being redrawn
+	 * @param mask   Bitmask which describes how the screen is being redrawn'
+	 */
+	virtual void glPaintTransformedOutput (const GLScreenPaintAttrib &attrib,
+					       const GLMatrix 		 &mask,
+					       const CompRegion 	 &region,
+					       CompOutput 		 *output,
+					       unsigned int		 mask);
+
+	/**
+	 * Hookable function to apply elements from a GLScreenPaintAttrib
+	 * to a GLMatrix in the context of a CompOutput
+	 *
+	 * @param attrib Describes the basic drawing attribs of a screen
+	 * including translation, rotation and scale to be applies to a matrix
+	 * @param output Describes the output in which these operations take
+	 * place
+	 * @param matrix Pointer to a matrix where transformations will
+	 * be applied
+	 */
+	virtual void glApplyTransform (const GLScreenPaintAttrib &attrib,
+				       CompOutput 		 *output,
+				       GLMatrix 		 *mask);
 
 	virtual void glEnableOutputClipping (const GLMatrix &,
 					     const CompRegion &,
@@ -266,22 +314,51 @@ class GLScreen :
 	CompOption::Vector & getOptions ();
         bool setOption (const CompString &name, CompOption::Value &value);
 
+	/**
+	 * Returns the current compiz-wide openGL texture filter
+	 */
 	GLenum textureFilter ();
+
+	/**
+	 * Sets a new compiz-wide openGL texture filter
+	 */
 	void setTextureFilter (GLenum);
 
 	void clearTargetOutput (unsigned int mask);
 
+	/**
+	 * Gets the libGL address of a particular openGL functor
+	 */
 	GL::FuncPtr getProcAddress (const char *name);
 
 	void updateBackground ();
 
+	/**
+	 * Returns the current compiz-wide texture filter
+	 */
 	GLTexture::Filter filter (int);
+
+	/**
+	 * Sets a new compiz-wide texture filter
+	 */
 	void setFilter (int, GLTexture::Filter);
 
 	GLFragment::Storage * fragmentStorage ();
 
+	/**
+	 * Sets a new compiz-wid openGL texture environment mode
+	 */
 	void setTexEnvMode (GLenum mode);
+
+	/**
+	 * Turns lighting on and off
+	 */
+
 	void setLighting (bool lighting);
+
+	/**
+	 * Returns true if lighting is enabled
+	 */
 	bool lighting ();
 
 	void clearOutput (CompOutput *output, unsigned int mask);
@@ -293,10 +370,17 @@ class GLScreen :
 
 	GLFBConfig * glxPixmapFBConfig (unsigned int depth);
 
+	/**
+	 * Returns a default icon texture
+	 */
 	GLTexture *defaultIcon ();
 
 	void resetRasterPos ();
 
+	/**
+	 * Returns a 4x4 const float array which
+	 * represents the current projection matrix
+	 */
 	const float * projectionMatrix ();
 
 	WRAPABLE_HND (0, GLScreenInterface, bool, glPaintOutput,
@@ -335,14 +419,61 @@ class GLWindowInterface :
     public WrapableInterface<GLWindow, GLWindowInterface>
 {
     public:
-	virtual bool glPaint (const GLWindowPaintAttrib &, const GLMatrix &,
-			      const CompRegion &, unsigned int);
-	virtual bool glDraw (const GLMatrix &, GLFragment::Attrib &,
-			     const CompRegion &, unsigned int);
-	virtual void glAddGeometry (const GLTexture::MatrixList &,
-				    const CompRegion &,const CompRegion &,
-				    unsigned int = MAXSHORT,
-				    unsigned int = MAXSHORT);
+
+	/**
+	 * Hookable function to paint a window on-screen
+	 *
+	 * @param attrib Describes basic drawing attribs of this window;
+	 * opacity, brightness, saturation
+	 * @param matrix A 4x4 matrix which describes the transformation of
+	 * this window
+	 * @param region Describes the region of the window being drawn
+	 * @param mask   Bitmask which describes how this window is drawn
+	 */
+	virtual bool glPaint (const GLWindowPaintAttrib &attrib,
+			      const GLMatrix 		&matrix,
+			      const CompRegion 		&region,
+			      unsigned int		mask);
+
+	/**
+	 * Hookable function to draw a window on-screen
+	 *
+	 * Unlike glPaint, when glDraw is called, the window is
+	 * drawn immediately
+	 *
+	 * @param matrix A 4x4 matrix which describes the transformation of
+	 * this window
+	 * @param attrib A Fragment attrib which describes the texture
+	 * modification state of this window
+	 * @param region Describes which region will be drawn
+	 * @param mask   Bitmask which describes how this window is drawn
+	 */
+	virtual bool glDraw (const GLMatrix 	&matrix,
+			     GLFragment::Attrib &attrib,
+			     const CompRegion 	&region,
+			     unsigned int	mask);
+
+	/**
+	 * Hookable function to add points to a window
+	 * texture geometry
+	 *
+	 * This function adds rects to a window's texture geometry
+	 * and modifies their points by the values in the GLTexture::MatrixList
+	 *
+	 * It is used for texture transformation to set points
+	 * for where the texture should be skewed
+	 *
+	 * @param matrices Describes the matrices by which the texture exists
+	 * @param region
+	 * @param clipRegion
+	 * @param min
+	 * @param max
+	 */
+	virtual void glAddGeometry (const GLTexture::MatrixList &matrices,
+				    const CompRegion 		&region,
+				    const CompRegion 		&clipRegion,
+				    unsigned int		min = MAXSHORT,
+				    unsigned int		max = MAXSHORT);
 	virtual void glDrawTexture (GLTexture *texture, GLFragment::Attrib &,
 				    unsigned int);
 	virtual void glDrawGeometry ();
@@ -354,6 +485,10 @@ class GLWindow :
 {
     public:
 
+	/**
+	 * Class which describes the texture geometry and transformation points
+	 * of a window
+	 */
 	class Geometry {
 	    public:
 		Geometry ();
@@ -361,7 +496,14 @@ class GLWindow :
 
 		void reset ();
 
+		/**
+		 * Set the number of vertices in the texture geometry
+		 */
 		bool moreVertices (int newSize);
+
+		/**
+		 * Set the number of indices in the texture geometry
+		 */
 		bool moreIndices (int newSize);
 
 	    public:
@@ -384,19 +526,43 @@ class GLWindow :
 
 	const CompRegion & clip () const;
 
+	/**
+	 * Returns the current paint attributes for this window
+	 */
 	GLWindowPaintAttrib & paintAttrib ();
+
+	/**
+	 * Returns the last paint attributes for this window
+	 */
 	GLWindowPaintAttrib & lastPaintAttrib ();
 
 	unsigned int lastMask () const;
 
+	/**
+	 * Binds this window to an openGL texture
+	 */
 	bool bind ();
+
+	/**
+	 * Releases this window from an openGL texture
+	 */
 	void release ();
 
+	/**
+	 * Returns the tiled textures for this window
+	 */
 	const GLTexture::List &       textures () const;
+
+	/**
+	 * Returns the matrices for the tiled textures for this windwo
+	 */
 	const GLTexture::MatrixList & matrices () const;
 
 	void updatePaintAttribs ();
 
+	/**
+	 * Returns the window texture geometry
+	 */
 	Geometry & geometry ();
 
 	GLTexture *getIcon (int width, int height);

plugins/opengl/include/opengl/texture.h
@@ -38,16 +38,31 @@
 
 #define POWER_OF_TWO(v) ((v & (v - 1)) == 0)
 
-#define COMP_TEX_COORD_X(m, vx) ((m).xx * (vx) + (m).x0)
+/**
+ * Returns a 2D matrix adjusted texture co-ordinate x
+ */
+#define COMP_TEX_COORD_X(m, vx) 
+/**
+ * Returns a 2D matrix adjusted texture co-ordinate y
+ */
 #define COMP_TEX_COORD_Y(m, vy) ((m).yy * (vy) + (m).y0)
 
+/**
+ * Returns a 2D matrix adjusted texture co-ordinate xy
+ */
 #define COMP_TEX_COORD_XY(m, vx, vy)		\
     ((m).xx * (vx) + (m).xy * (vy) + (m).x0)
+/**
+ * Returns a 2D matrix adjusted texture co-ordinate yx
+ */
 #define COMP_TEX_COORD_YX(m, vx, vy)		\
     ((m).yx * (vx) + (m).yy * (vy) + (m).y0)
 
 class PrivateTexture;
 
+/**
+ * Class which represents an openGL texture
+ */
 class GLTexture : public CompRect {
     public:
 
@@ -64,6 +79,10 @@ class GLTexture : public CompRect {
 
 	typedef std::vector<Matrix> MatrixList;
 
+	/**
+	 * Class which represents a list of openGL textures,
+	 * usually used for texture tiling
+	 */
 	class List : public std::vector <GLTexture *> {
 
 	    public:
@@ -81,28 +100,93 @@ class GLTexture : public CompRect {
 	typedef unsigned int BindPixmapHandle;
 
     public:
+
+	/**
+	 * Returns the openGL texture name
+	 */
 	GLuint name () const;
+
+	/**
+	 * Returns the openGL texture target
+	 */
 	GLenum target () const;
+
+	/**
+	 * Returns the openGL texture filter
+	 */
 	GLenum filter () const;
 
+	/**
+	 * Returns a 2D 2x3 matrix describing the transformation of
+	 * the texture
+	 */
 	const Matrix & matrix () const;
 
+	/**
+	 * Establishes the texture as the current drawing texture
+	 * in the openGL context
+	 *
+	 * @param filter Defines what kind of filtering level this
+	 * texture should be drawn with
+	 */
 	virtual void enable (Filter filter);
+
+	/**
+	 * Stops the textures from being the current drawing texture
+	 * in the openGL context
+	 */
 	virtual void disable ();
 
+	/**
+	 * Returns true if this texture is MipMapped
+	 */
 	bool mipmap () const;
+
+	/**
+	 * Sets if this texture should be MipMapped
+	 */
 	void setMipmap (bool);
+
+	/**
+	 * Sets the openGL filter which should be used on this
+	 * texture
+	 */
 	void setFilter (GLenum);
 	void setWrap (GLenum);
 
+	/**
+	 * Increases the reference count of a texture
+	 */
 	static void incRef (GLTexture *);
+
+	/**
+	 * Decreases the reference count of a texture
+	 */
 	static void decRef (GLTexture *);
 
+	/**
+	 * Returns a GLTexture::List with the contents of
+	 * some pixmap
+	 *
+	 * @param pixmap Specifies the pixmap data which should be converted
+	 * into texture data
+	 * @param width Specifies the width of the texture
+	 * @param height Specifies the height of the texture
+	 * @param depth Specifies the color depth of the texture
+	 */
 	static List bindPixmapToTexture (Pixmap pixmap,
 					 int width,
 					 int height,
 					 int depth);
 
+	/**
+	 * Returns a GLTexture::List with the contents of of
+	 * a raw image buffer
+	 *
+	 * @param image Specifies a raw image buffer which should be converted
+	 * into texture data
+	 * @param size Specifies the size of this new texture
+	 */
 	static List imageBufferToTexture (const char *image,
 					  CompSize   size);
 
@@ -111,6 +195,13 @@ class GLTexture : public CompRect {
 					GLenum     format,
 					GLenum     type);
 
+	/**
+	 * Uses image loading plugins to read an image from the disk and
+	 * return a GLTexture::List with its contents
+	 *
+	 * @param imageFileName The filename of the image
+	 * @param size		The size of this new texture
+	 */
 	static List readImageToTexture (CompString &imageFileName,
 					CompSize   &size);
 

plugins/opengl/include/opengl/fragment.h
@@ -37,6 +37,10 @@
 struct GLWindowPaintAttrib;
 class GLScreen;
 
+/**
+ * Describes a texture modification fragment program
+ * for a texture
+ */
 namespace GLFragment {
 
     class Storage;
@@ -51,6 +55,10 @@ namespace GLFragment {
 	    FunctionData ();
 	    ~FunctionData ();
 
+	    /**
+	     * Returns the status of this fragment program
+	     * (valid or invalid)
+	     */
 	    bool status ();
 
 	    void addTempHeaderOp (const char *name);

plugins/opengl/include/opengl/vector.h
@@ -26,6 +26,10 @@
 #ifndef _GLVECTOR_H
 #define _GLVECTOR_H
 
+/**
+ * Class which describes a point or vector
+ * in 3D space
+ */
 class GLVector {
     public:
 	typedef enum {
@@ -38,20 +42,66 @@ class GLVector {
 	GLVector ();
 	GLVector (float x, float y, float z, float w);
 
+	/**
+	 * Returns a reference to the x, y, z or w value by using
+	 * 0, 1, 2, 3 as array-access items
+	 */
 	float& operator[] (int item);
+
+	/**
+	 * Returns a reference to the x, y, z or w value by using
+	 * x, y, z, w as array-access items
+	 */
 	float& operator[] (VectorCoordsEnum coord);
 
+	/**
+	 * Returns a readonly x, y, z or w value by using
+	 * 0, 1, 2, 3 as array-access items
+	 */
 	const float operator[] (int item) const;
+
+	/**
+	 * Returns a readonly x, y, z or w value by using
+	 * x, y, z, w as array-access items
+	 */
 	const float operator[] (VectorCoordsEnum coord) const;
 
+	/**
+	 * Adds all elements in a GLVector
+	 */
 	GLVector& operator+= (const GLVector& rhs);
+
+	/**
+	 * Subtracts all elements in a GLVector
+	 */
 	GLVector& operator-= (const GLVector& rhs);
+
+	/**
+	 * Scales all elements in a vector
+	 * @param k Scale factor
+	 */
 	GLVector& operator*= (const float k);
+
+	/**
+	 * Scales all elements in a vector by 1 / k
+	 * @param k Scale factor
+	 */
 	GLVector& operator/= (const float k);
 	GLVector& operator^= (const GLVector& rhs);
 
+	/**
+	 * Returns the norm of this vector
+	 */
 	float norm ();
+
+	/**
+	 * Returns the normalized version of the vector
+	 */
 	GLVector& normalize ();
+	
+	/**
+	 * Returns the homogenized version of the vector
+	 */
 	GLVector& homogenize ();
 
     private:

p12tic/compiz-mirror

commit 09b864dbb1144ad031f66016cb60b0541b53dcf6

Fix build. Errors introduced by 69c7f0cf084359d666447f4573d7cc8d8b6420d0

plugins/opengl/include/opengl/opengl.h
@@ -274,7 +274,7 @@ class GLScreenInterface :
 	 * @param mask   Bitmask which describes how the screen is being redrawn'
 	 */
 	virtual void glPaintTransformedOutput (const GLScreenPaintAttrib &attrib,
-					       const GLMatrix 		 &mask,
+					       const GLMatrix 		 &matrix,
 					       const CompRegion 	 &region,
 					       CompOutput 		 *output,
 					       unsigned int		 mask);

plugins/opengl/include/opengl/texture.h
@@ -41,7 +41,7 @@
 /**
  * Returns a 2D matrix adjusted texture co-ordinate x
  */
-#define COMP_TEX_COORD_X(m, vx) 
+#define COMP_TEX_COORD_X(m, vx) ((m).xx * (vx) + (m).x0)
 /**
  * Returns a 2D matrix adjusted texture co-ordinate y
  */